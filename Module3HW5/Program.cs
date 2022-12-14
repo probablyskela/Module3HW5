27c65bfd236976071906cb539584ac3ac38c67d1
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2022-12-14 19:03:06 +0200

    Small fix

--- Module3HW5/Program.cs
+++ Module3HW5/Program.cs
@@ -3,9 +3,9 @@
     return await Task.Run(() => string.Join(string.Empty, File.ReadAllLines(path)));
 }
 
-static async Task<string> Concatenate(params string[] paths)
+static async Task<string> Concatenate(string separator, IEnumerable<string> paths)
 {
-    return string.Join(string.Empty, await Task.WhenAll(paths.Select(ReadFile).ToArray()));
+    return string.Join(separator, await Task.WhenAll(paths.Select(ReadFile).ToArray()));
 }
 
-Console.WriteLine(await Concatenate("../../../hello.txt", "../../../world.txt"));
+Console.WriteLine(await Concatenate(" ", new[] { "../../../hello.txt", "../../../world.txt" }));
