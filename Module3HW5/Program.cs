3a2d25334bc8f3d14057bb9a555087285fde7b0c
Oleksandr Yovbak <oleksandr.yovbak@gmail.com>
2022-12-14 19:00:07 +0200

    Homework done

--- Module3HW5/Program.cs
+++ Module3HW5/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿static async Task<string> ReadFile(string path)
+{
+    return await Task.Run(() => string.Join(string.Empty, File.ReadAllLines(path)));
+}
 
-Console.WriteLine("Hello, World!");
+static async Task<string> Concatenate(params string[] paths)
+{
+    return string.Join(string.Empty, await Task.WhenAll(paths.Select(ReadFile).ToArray()));
+}
+
+Console.WriteLine(await Concatenate("../../../hello.txt", "../../../world.txt"));
